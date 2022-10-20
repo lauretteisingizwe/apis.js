7c74fb372421e7f93746410f250db9619a42e7bf
unknown <isinginzwe123@gmail.com>
2022-10-20 17:42:23 +0200

    added login endpoint and validations

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
-require ('dotenv').config()
+require("dotenv").config();
 const port = 3300;
 
 const { celebrate, Joi, errors, Segments } = require("celebrate");
@@ -39,7 +39,7 @@ app.get("/users/:id", (req, res) => {
 });
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
-app.post("/users", async (req, res) => {
+app.post("/rregister", async (req, res) => {
   const user = req.body;
   console.log(req.body);
 
@@ -65,11 +65,10 @@ app.post("/users", async (req, res) => {
         error: "Email already exists.",
       });
     }
-  
+
     // Create token
-    const accessToken= jwt.sign(user, proccess.env.ACCESS_SECRET_TOKEN)
-    res.json({accessToken: accessToken})
-    
+    const accessToken = jwt.sign(user, proccess.env.ACCESS_SECRET_TOKEN);
+    res.json({ accessToken: accessToken });
   } catch (err) {
     console.log(err);
     res.status(400).json({
@@ -109,4 +108,51 @@ app.post("/users", async (req, res) => {
     });
     client.end;
   });
+});
+//
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const data = await client.query(`SELECT * FROM users WHERE email= $1;`, [
+      email,
+    ]); //Verifying if the user exists in the database
+    const user = data.rows;
+    if (user.length === 0) {
+      res.status(400).json({
+        error: "User is not registered, Sign Up first",
+      });
+    } else {
+      bcrypt.compare(password, user[0].password, (err, result) => {
+        //Comparing the hashed password
+        if (err) {
+          res.status(500).json({
+            error: "Server error",
+          });
+        } else if (result === true) {
+          //Checking if credentials match
+          const token = jwt.sign(
+            {
+              email: email,
+            },
+            process.env.SECRET_KEY
+          );
+          res.status(200).json({
+            message: "User signed in!",
+            token: token,
+          });
+        } else {
+          //Declaring the errors
+          if (result != true)
+            res.status(400).json({
+              error: "Enter correct password!",
+            });
+        }
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      error: "Database error occurred while signing in!", //Database connection error
+    });
+  }
 });
